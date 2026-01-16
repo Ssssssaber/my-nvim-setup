bfa4fd075d026856a76a869d6854d3589dde92de
Ssssssaber <bladimir.lobanov@gmail.com>
2026-01-16 13:56:47 +0700

    (update): removed options that are not needed for vscode

--- init.lua
+++ init.lua
@@ -2,18 +2,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Preferences
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.wrap = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
-vim.opt.mouse = "a"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.undofile = true
-vim.opt.backup = true
 vim.opt.hlsearch = true
 vim.opt.inccommand = "split"
 vim.opt.list = true
